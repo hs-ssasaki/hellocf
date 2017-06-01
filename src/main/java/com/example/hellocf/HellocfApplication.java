aa57db5886d4e2be3a2f3a45ce11c19947cbb1aa
Shinya Sasaki <shinya.sasaki.sq@gmail.com>
2017-06-02 01:12:29 +0900

    Bind Redis service using VCAP_SERVICES application-cloud.properties

--- src/main/java/com/example/hellocf/HellocfApplication.java
+++ src/main/java/com/example/hellocf/HellocfApplication.java
@@ -1,13 +1,22 @@
 package com.example.hellocf;
 
+import java.time.OffsetDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableCaching
 public class HellocfApplication {
+	@Autowired
+	Greeter greeter;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HellocfApplication.class, args);
@@ -15,6 +24,14 @@ public class HellocfApplication {
 	
 	@GetMapping("/hello")
 	public String index() {
-		return "Hello (" + System.getenv("CF_INSTANCE_INDEX") + ")";
+		return greeter.hello();
+	}
+}
+
+@Component
+class Greeter {
+	@Cacheable("hello") // 実行結果をキャッシュします
+	public String hello() {
+		return "Hello. It's " + OffsetDateTime.now() + " now.";
 	}
 }
