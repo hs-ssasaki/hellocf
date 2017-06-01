ee6a2ca7fa63fa46e60c4ab46626b6b896a352b9
Shinya Sasaki <shinya.sasaki.sq@gmail.com>
2017-06-01 23:17:56 +0900

    first commit

---
+++ src/main/java/com/example/hellocf/HellocfApplication.java
@@ -0,0 +1,20 @@
+package com.example.hellocf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HellocfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellocfApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	public String index() {
+		return "Hello (" + System.getenv("CF_INSTANCE_INDEX") + ")";
+	}
+}
